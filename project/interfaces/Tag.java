01e01cc60f4d4e49bb87c0f5ef228caf3f9d61a0
Robert Schaedler III <robertschaedler3@gmail.com>
2020-09-21 15:44:43 -0400

    fix MARR typo

--- project/interfaces/Tag.java
+++ project/interfaces/Tag.java
@@ -1,5 +1,5 @@
 package project.interfaces;
 
 public enum Tag {
-    INDI, NAME, SEX, BIRT, DEAT, FAMC, FAMS, FAM, MAR, HUSB, WIFE, CHIL, DIV, DATE, HEAD, TRLR, NOTE
+    INDI, NAME, SEX, BIRT, DEAT, FAMC, FAMS, FAM, MARR, HUSB, WIFE, CHIL, DIV, DATE, HEAD, TRLR, NOTE, INVALID
 }
