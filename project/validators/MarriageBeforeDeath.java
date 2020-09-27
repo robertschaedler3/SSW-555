80f74c8b89c3925ba6b617c18d3b8d1a84b5473f
Allen <ajbest91@gmail.com>
2020-09-27 04:34:21 -0400

    check if death happens before marriage

--- project/validators/MarriageBeforeDeath.java
+++ project/validators/MarriageBeforeDeath.java
@@ -1,7 +1,11 @@
 package project.validators;
 
+import java.util.Map;
+
 import project.Validator;
 import project.models.GEDFile;
+import project.models.Family;
+import project.models.Individual;
 
 public class MarriageBeforeDeath extends Validator {
 
@@ -11,7 +15,18 @@ public class MarriageBeforeDeath extends Validator {
 
     protected boolean check(GEDFile gedFile) {
         boolean valid = true;
-        
+        Map<String, Individual> individuals = gedFile.getIndividuals();
+
+        for (Map.Entry<String, Family> entry : gedFile.getFamilies().entrySet()) {
+            Family family = entry.getValue();
+            Individual father = individuals.get(family.getHusband());
+            Individual wife = individuals.get(family.getWife());
+            if((father.getDeath().after(family.getMarriage())) || (wife.getDeath().after(family.getMarriage()))){
+                System.out.printf("Death of parent occurs before of marriage in family %s", family.getID());
+                valid = false;
+            }
+        }
+
         return valid;
     }
 
