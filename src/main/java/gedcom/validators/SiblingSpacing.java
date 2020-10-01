9db247fdfda99df01059bb6e93c515dea5055ea3
Robert Schaedler III <robertschaedler3@gmail.com>
2020-10-01 14:27:58 -0400

    add check for sibling spacing

--- src/main/java/gedcom/validators/SiblingSpacing.java
+++ src/main/java/gedcom/validators/SiblingSpacing.java
@@ -1,7 +1,12 @@
 package gedcom.validators;
 
+import java.util.List;
+import java.util.Map;
+
 import gedcom.Validator;
+import gedcom.models.Family;
 import gedcom.models.GEDFile;
+import gedcom.models.Individual;
 
 public class SiblingSpacing extends Validator {
 
@@ -9,8 +14,30 @@ public class SiblingSpacing extends Validator {
         super(validator);
     }
 
-    protected boolean check(GEDFile gedfile) {
-        return true;
+    protected boolean check(GEDFile gedFile) {
+        boolean valid = true;
+        Map<String, Individual> individuals = gedFile.getIndividuals();
+
+        for (Map.Entry<String, Family> entry : gedFile.getFamilies().entrySet()) {
+            Family family = entry.getValue();
+            List<String> siblings = family.getChildren();
+            for (int i = 0; i < siblings.size(); i++) {
+                Individual individual = individuals.get(siblings.get(i));
+                for (int j = i + 1; (individual.getBirthday() != null) && (j < siblings.size()); j++) {
+                    Individual sibling = individuals.get(siblings.get(j));
+                    if (sibling.getBirthday() != null) {
+                        long diff = Math.abs(individual.getBirthday().getTime() - sibling.getBirthday().getTime());
+                        long diffInDays = (diff / (1000 * 60 * 60 * 24)) % 365;
+                        if (diffInDays > 2 && diffInDays < (30 * 8)) {
+                            System.out.printf("Individual %s was born more the 2 days and less than 8 months from individual %s.\n", individual.getID(), sibling.getID());
+                            valid = false;
+                        }
+                    }
+                }
+            }
+        }
+
+        return valid;
     }
 
 }
