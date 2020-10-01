400a2404db42c64f489841e49e25ed237b416446
Robert Schaedler III <robertschaedler3@gmail.com>
2020-10-01 14:35:45 -0400

    migrate sibling spacing to valid birth loop

--- src/main/java/gedcom/validators/ValidBirth.java
+++ src/main/java/gedcom/validators/ValidBirth.java
@@ -1,6 +1,7 @@
 package gedcom.validators;
 
 import java.util.Calendar;
+import java.util.List;
 import java.util.Map;
 
 import gedcom.Validator;
@@ -20,19 +21,18 @@ public class ValidBirth extends Validator {
 
         for (Map.Entry<String, Family> entry : gedFile.getFamilies().entrySet()) {
             Family family = entry.getValue();
-            for (String childID : family.getChildren()) {
-                Individual child = individuals.get(childID);
+            List<String> children = family.getChildren(); 
+            for (int i = 0; i < children.size(); i++) {
+                Individual child = individuals.get(children.get(i));
                 Individual mother = individuals.get(family.getWife());
                 Individual father = individuals.get(family.getHusband());
                 if (child.getBirthday() != null) {
                     if (family.getMarriage() != null && child.getBirthday().before(family.getMarriage())) {
-                        System.out.printf("Birth of child %s occurs before of marriage in family %s\n", child.getID(),
-                                family.getID());
+                        System.out.printf("Birth of child %s occurs before of marriage in family %s\n", child.getID(), family.getID());
                         valid = false;
                     }
                     if (mother.getDeath() != null && child.getBirthday().after(mother.getDeath())) {
-                        System.out.printf("Birth of child %s occurs after death of mother %s\n", child.getID(),
-                                mother.getID());
+                        System.out.printf("Birth of child %s occurs after death of mother %s\n", child.getID(), mother.getID());
                         valid = false;
                     }
                     if (father.getDeath() != null) {
@@ -40,9 +40,19 @@ public class ValidBirth extends Validator {
                         calendar.setTime(father.getDeath());
                         calendar.add(Calendar.MONTH, 9);
                         if (child.getBirthday().after(calendar.getTime())) {
-                            System.out.printf("Birth of child %s occurs 9+ months after death of father %s\n",
-                                    child.getID(), father.getID());
+                            System.out.printf("Birth of child %s occurs 9+ months after death of father %s\n", child.getID(), father.getID());
                             valid = false;
+                        }
+                    }
+                    for (int j = i + 1; j < children.size(); j++) {
+                        Individual sibling = individuals.get(children.get(j));
+                        if (sibling.getBirthday() != null) {
+                            long diff = Math.abs(child.getBirthday().getTime() - sibling.getBirthday().getTime());
+                            long diffInDays = (diff / (1000 * 60 * 60 * 24)) % 365;
+                            if (diffInDays > 2 && diffInDays < (30 * 8)) {
+                                System.out.printf("Individual %s was born more the 2 days and less than 8 months from individual %s.\n", child.getID(), sibling.getID());
+                                valid = false;
+                            }
                         }
                     }
                 }
