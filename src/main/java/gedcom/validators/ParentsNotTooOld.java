9285dfdb2576b0b51fd9b6bf46f7022b23f6d4de
Robert Schaedler III <robertschaedler3@gmail.com>
2020-10-07 21:27:06 -0400

    fix issue with comparing age of parent and child

--- src/main/java/gedcom/validators/ParentsNotTooOld.java
+++ src/main/java/gedcom/validators/ParentsNotTooOld.java
@@ -1,36 +1,56 @@
 package gedcom.validators;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.Date;
 
 import gedcom.Validator;
-import gedcom.interfaces.Gender;
+import gedcom.models.Family;
 import gedcom.models.GEDFile;
 import gedcom.models.Individual;
 
 public class ParentsNotTooOld extends Validator {
 
+	private final int FATHER_THRESHOLD = 80;
+	private final int MOTHER_THRESHOLD = 60;
+
 	public ParentsNotTooOld(Validator validator) {
 		super(validator);
+	}
+
+	private int parentAgeWhenChildBorn(Individual parent, Individual child) {
+		long diffInYears = 0;
+		if (parent == null || child == null) {
+			return (int) diffInYears;
+		}
+
+		Date parentBirthday = parent.getBirthday();
+		Date childBirthday = child.getBirthday();
+		if (parentBirthday != null && childBirthday != null && parentBirthday.before(childBirthday)) {
+			diffInYears = (childBirthday.getTime() - parentBirthday.getTime()) / (1000l * 60 * 60 * 24 * 365);
+		}
+		System.out.println(diffInYears);
+		return (int) diffInYears;
 	}
 
 	protected boolean check(GEDFile gedFile) {
 		boolean valid = true;
 
-		List<Individual> individuals = gedFile.getIndividuals();
+		for (Family family : gedFile.getFamilies()) {
 
-		for (Individual parent : individuals) {
-			Collection<Individual> children = parent.getChildren();
-			if (children != null && children.size() > 0) {
-				int threshold = parent.getGender() == Gender.M ? 80 : 60;
+			Individual father = family.getHusband();
+			Individual mother = family.getWife();
 
-				for (Individual child : children) {
-					if (parent.age() - child.age() > threshold) {
-						System.out.println("Anomaly US12: Parent too old: Parent " + parent.getName() + "("
-								+ parent.getID() + ") is more than " + threshold + " years older than child "
-								+ child.getName() + "(" + child.getID() + ")");
-						valid = false;
-					}
+			for (Individual child : family.getChildren()) {
+				if (parentAgeWhenChildBorn(father, child) > FATHER_THRESHOLD) {
+					System.out.println("Anomaly US12: Father too old: Parent " + father.getName() + "(" + father.getID()
+							+ ") is more than " + FATHER_THRESHOLD + " years older than child " + child.getName() + "("
+							+ child.getID() + ")");
+					valid = false;
+				}
+				if (parentAgeWhenChildBorn(mother, child) > MOTHER_THRESHOLD) {
+					System.out.println("Anomaly US12: Mother too old: Parent " + mother.getName() + "(" + mother.getID()
+							+ ") is more than " + MOTHER_THRESHOLD + " years older than child " + child.getName() + "("
+							+ child.getID() + ")");
+					valid = false;
 				}
 			}
 		}
