db7f7a10f87c8a2f15cd6ab4e996bfa28d03f0ed
Robert Schaedler III <robertschaedler3@gmail.com>
2020-10-07 21:27:20 -0400

    fix family linking

--- src/test/java/gedcom/TestParentsNotTooOld.java
+++ src/test/java/gedcom/TestParentsNotTooOld.java
@@ -69,12 +69,13 @@ public class TestParentsNotTooOld {
 			grandchild.addChildFamily(family2);
 
 			Individual[] individuals2 = { child1, father1, mother1, child2, grandchild };
-			family2.addChild(child1);
-			family2.setHusband(father1);
-			family2.setWife(mother1);
-			family2.addChild(child2);
+			family1.addChild(child1);
+			family1.setHusband(father1);
+			family1.setWife(mother1);
+			family1.addChild(child2);
+			family2.setHusband(child2);
 			family2.addChild(grandchild);
-			Family[] families2 = { new Family("Family1"), new Family("Family2") };
+			Family[] families2 = { family1, family2 };
 			// test with two families - a grandchild added
 			assertTrue(parentsNotTooOldValidator.isValid(new GEDFile(individuals2, families2)));
 
