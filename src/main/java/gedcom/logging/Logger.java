1418bd68bc4f84c8eff759ebb788d4d9a71c6319
Robert Schaedler III <robertschaedler3@gmail.com>
2020-10-13 19:58:52 -0400

    create baseline logger structure

--- src/main/java/gedcom/logging/Logger.java
+++ src/main/java/gedcom/logging/Logger.java
@@ -2,4 +2,18 @@ package gedcom.logging;
 
 public class Logger {
 
+    private static boolean raiseErrors = false;
+
+    public static void logError() {
+        // TODO
+    }
+
+    public static void logAnomaly() {
+        // TODO
+    }
+
+    private static void log() {
+        // TODO
+    }
+
 }
