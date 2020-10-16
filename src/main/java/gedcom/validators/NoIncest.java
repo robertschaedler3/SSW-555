57425ac8907769275365119533967d57183b917d
Robert Schaedler III <robertschaedler3@gmail.com>
2020-10-16 01:02:38 -0400

    add null checks

--- src/main/java/gedcom/validators/NoIncest.java
+++ src/main/java/gedcom/validators/NoIncest.java
@@ -15,29 +15,30 @@ public class NoIncest extends Validator {
 
 		for (Family family : gedFile.getFamilies()) {
             Individual husband = family.getHusband();
-            Individual wife = family.getWife();
-            if (husband.isSibling(wife)) {
-                System.out.printf("Anomaly US18: Siblings %s and %s are married, in family %s.\n", husband, wife, family);
-                valid = false;
-            } else if (husband.getCousins(1).contains(wife)) {
-                System.out.printf("Anomaly US19: First cousins %s and %s are married, in family %s.\n", husband, wife, family);
-                valid = false;
-            } 
-			
-			for (Individual sibling : husband.getSiblings()) {
-				if (sibling.getChildren().contains(wife)) {
-					System.out.printf("Anomaly US20: Uncle %s is married to their niece %s.", husband, wife);
+			Individual wife = family.getWife();
+			if (husband != null && wife != null) {
+				if (husband.isSibling(wife)) {
+					System.out.printf("Anomaly US18: Siblings %s and %s are married, in family %s.\n", husband, wife, family);
 					valid = false;
+				} else if (husband.getCousins(1).contains(wife)) {
+					System.out.printf("Anomaly US19: First cousins %s and %s are married, in family %s.\n", husband, wife, family);
+					valid = false;
+				} 
+				
+				for (Individual sibling : husband.getSiblings()) {
+					if (sibling.getChildren().contains(wife)) {
+						System.out.printf("Anomaly US20: Uncle %s is married to their niece %s.", husband, wife);
+						valid = false;
+					}
+				}
+				
+				for (Individual sibling : wife.getSiblings()) {
+					if (sibling.getChildren().contains(husband)) {
+						System.out.printf("Anomaly US20: Aunt %s is married to their nephew %s.", wife, husband);
+						valid = false;
+					}
 				}
 			}
-			
-			for (Individual sibling : wife.getSiblings()) {
-				if (sibling.getChildren().contains(husband)) {
-					System.out.printf("Anomaly US20: Aunt %s is married to their nephew %s.", wife, husband);
-					valid = false;
-				}
-			}
-			
 		}
 		
 		return valid;
