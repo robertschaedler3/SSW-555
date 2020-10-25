8343b829bfa3e2462eff5fd9e797e125c2ab1c7d
Constance  Xu <cxu16@stevens.edu>
2020-10-24 20:44:57 -0400

    user story 24

---
+++ src/main/java/gedcom/validators/uniqueFamiliesBySpouses.java
@@ -0,0 +1,32 @@
+package gedcom.validators;
+
+import gedcom.models.*;
+import java.util.List;
+/**
+ * No more than one family with the same spouses by name 
+ * and the same marriage date should appear in a GEDCOM file
+ */
+public class uniqueFamiliesBySpouses extends Validator{
+    public uniqueFamiliesBySpouses(Validator validator) {
+        super(validator);
+    }
+
+    protected boolean check(GEDFile gedFile) {
+        boolean valid = true;
+
+        List<Family> families = gedFile.getFamilies();
+
+        for (int i = 0; i < families.size(); i++) {
+            Family current = families.get(i);
+            for (int j = i+1; j < families.size(); j++) {
+                Family check = families.get(j);
+                if (current.getMarriage() == check.getMarriage() && current.getHusband() == check.getHusband() && current.getWife() == check.getWife()) {
+                    System.out.println("Anomaly US24: No more than one family with the same spouses by name and the same marriage date should appear in GEDCOM file");
+                    valid = false;
+                }
+            }
+        }
+        return valid;
+
+    }
+}
