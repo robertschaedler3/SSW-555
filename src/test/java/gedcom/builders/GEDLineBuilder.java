299d2b2b19718021481c3683a154e845310403c7
Robert Schaedler III <robertschaedler3@gmail.com>
2020-10-27 16:31:13 -0400

    add build methods for different line scenarios

--- src/test/java/gedcom/builders/GEDLineBuilder.java
+++ src/test/java/gedcom/builders/GEDLineBuilder.java
@@ -1,5 +1,21 @@
 package gedcom.builders;
 
+import gedcom.interfaces.Tag;
+
 public class GEDLineBuilder {
+
+    private static String LINE_FORMAT = "%d %s %s\n";
+
+    public static String build(int level, Tag tag) {
+        return String.format(LINE_FORMAT, level, tag, " ");
+    }
+
+    public static String build(int level, Tag tag, String args) {
+        return String.format(LINE_FORMAT, level, tag, args);
+    }
+
+    public static String build(int level, String ID, Tag tag) {
+        return String.format(LINE_FORMAT, level, ID, tag);
+    }
 
 }
