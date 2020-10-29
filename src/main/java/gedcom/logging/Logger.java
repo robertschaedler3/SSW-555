de7b298a4daa6027b1a5773998559bc456dbdf48
Robert Schaedler III <robertschaedler3@gmail.com>
2020-10-28 23:15:09 -0400

    update logging to include variable line number and context

--- src/main/java/gedcom/logging/Logger.java
+++ src/main/java/gedcom/logging/Logger.java
@@ -1,17 +1,49 @@
 package gedcom.logging;
 
+import java.util.Arrays;
+
+import gedcom.models.GEDObject;
+
 public class Logger {
 
-    private static String MESSAGE_FORMAT = "[LINE %d] %s US%02d >> %s";
+    private static String MESSAGE_FORMAT = "%s US%02d %s>> %s %s";
+    private static String LINE_FORMAT = "[LINE %d] ";
 
-    public static void log(int line, Error error) {
-        String fullMessage = String.format(MESSAGE_FORMAT, line, error.type(), error.code(), error);
+    private static Logger logger = new Logger();
+
+    private Logger() {
+        // Use getInstance()
+    }
+
+    public static Logger getInstance() {
+        return logger;
+    }
+
+    private String line(int line) {
+        return String.format(LINE_FORMAT, line);
+    }
+
+    private String context(GEDObject... objs) {
+        return Arrays.asList(objs).toString();
+    }
+
+    public void log(Error error) {
+        String fullMessage = String.format(MESSAGE_FORMAT, error.type(), error.code(), "", error, "");
         System.out.println(fullMessage);
     }
 
-    public static void log(int line, Error error, String msgFmt, Object... args) {
-        String messageContext = String.format(MESSAGE_FORMAT, line, error.type(), error.code(), msgFmt);
-        String fullMessage = String.format(messageContext, args);
+    public void log(Error error, GEDObject... objs) {
+        String fullMessage = String.format(MESSAGE_FORMAT, error.type(), error.code(), "", error.message(), context(objs));
+        System.out.println(fullMessage);
+    }
+
+    public void log(int line, Error error) {
+        String fullMessage = String.format(MESSAGE_FORMAT, error.type(), error.code(), line(line), error, "");
+        System.out.println(fullMessage);
+    }
+
+    public void log(int line, Error error, GEDObject... objs) {
+        String fullMessage = String.format(MESSAGE_FORMAT, error.type(), error.code(), line(line), error.message(), context(objs));
         System.out.println(fullMessage);
     }
 
